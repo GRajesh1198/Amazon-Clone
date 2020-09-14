ee853938d638db63daf0ecc0eb5d7f707a92f6cf
GRajesh1198 <50822370+GRajesh1198@users.noreply.github.com>
2020-09-14 18:34:20 +0530

    Day 1 Completed

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Product.css";
 import { useStateValue } from "./StateProvider"
-function Product({ title, image, price, rating }) {
+function Product({ id,title, image, price, rating }) {
   const [{basket},dispatch] = useStateValue();
   console.log("This is the basket .>>>>> ", basket)
 
@@ -9,7 +9,7 @@ function Product({ title, image, price, rating }) {
     dispatch({
       type:'ADD_TO_BASKET',
       item:{
-        
+        id:id,
         title:title,
         image:image,
         price:price,
